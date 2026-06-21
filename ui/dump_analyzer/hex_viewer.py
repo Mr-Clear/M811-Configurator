c7d4a2eb139b211264ea4b502d1c84e4e1b04660
Thomas <git@thomas.klierlinge.de>
2026-06-21 21:38:05 +0200

    Enhance hex viewer with data editing capabilities

--- ui/dump_analyzer/hex_viewer.py
+++ ui/dump_analyzer/hex_viewer.py
@@ -4,12 +4,16 @@ from dataclasses import dataclass
 from enum import Enum
 
 from PySide6.QtCore import QEvent, QPoint, QPointF, QSize, QTimer, Signal
-from PySide6.QtGui import (QColor, QCursor, QFont, QFontMetrics, QMouseEvent,
-                           QPainter, QPaintEvent, QPalette, QResizeEvent,
-                           QShowEvent, QWheelEvent)
-from PySide6.QtWidgets import QWidget
+from PySide6.QtGui import (QColor, QCursor, QFont, QFontMetrics, QKeyEvent,
+                           QMouseEvent, QPainter, QPaintEvent, QPalette,
+                           QResizeEvent, QShowEvent, Qt, QWheelEvent)
+from PySide6.QtWidgets import QScrollArea, QWidget
 
 from ui.config import Config
+
+NUMBER_KEYS = set(range(Qt.Key.Key_0, Qt.Key.Key_9 + 1))
+LETTER_KEYS = set(range(Qt.Key.Key_A, Qt.Key.Key_F + 1))
+HEX_EDIT_KEYS = NUMBER_KEYS.union(LETTER_KEYS)
 
 def _blend_color(color1: QColor, color2: QColor, ratio: float = 0.5) -> QColor:
     r = int(color1.red() * (1 - ratio) + color2.red() * ratio)
@@ -27,6 +31,7 @@ class HexViewer(QWidget):
         HOVER = 1
         SELECTED = 2
         NULL_VALUE = 3
+        CHANGED = 4
 
     @dataclass
     class SizeHint:
@@ -38,10 +43,10 @@ class HexViewer(QWidget):
         def size(self) -> QSize:
             return QSize(self.width, self.height)
 
-    def __init__(self, data: bytes, parent: QWidget | None = None) -> None:
+    def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
+        self._data = bytearray()
         self._config = Config.instance()
-        self._data: bytes = data
         self._padding = 4
         self._current_size_hint: HexViewer.SizeHint = HexViewer.SizeHint(0, 0, 0)
         self._start_address: int = 0
@@ -53,6 +58,7 @@ class HexViewer(QWidget):
         self._hover_byte: int | None = None
         self._selected_byte: int | None = None
         self._colors: dict[HexViewer.Colors, QColor] = {}
+        self._edit_cursor_pos = 0
 
         self.resize(800, 600)
         font = self.font()
@@ -60,10 +66,17 @@ class HexViewer(QWidget):
         font.setFamily("Courier")
         self.setFont(font)
         self.setMouseTracking(True)
+        self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
 
-    def set_data(self, data: bytes) -> None:
+    @property
+    def data(self) -> memoryview:
+        '''Get the current data from the hex viewer.'''
+        return memoryview(self._data)
+    @data.setter
+    def data(self, data: bytes) -> None:
         '''Set the data to be displayed in the hex viewer.'''
-        self._data = data
+        self._data_source = data
+        self._data = bytearray(data)
         self._set_selected_byte(None)
         self._set_hover_byte(None)
         self._calculate_size()
@@ -95,20 +108,44 @@ class HexViewer(QWidget):
                 return byte_index
         return None
 
+    def ensure_selected_byte_visible(self) -> None:
+        if self._selected_byte is None:
+            return
+        area = self._get_scroll_area()
+        if area is None:
+            return
+        size_hint = self._current_size_hint
+        line = self._selected_byte // size_hint.bytes_per_line
+        line_y = line * self.fontMetrics().height()
+        if line_y < area.verticalScrollBar().value():
+            area.verticalScrollBar().setValue(line_y)
+        elif line_y + self.fontMetrics().height() > area.verticalScrollBar().value() + area.viewport().height():
+            area.verticalScrollBar().setValue(line_y + self.fontMetrics().height() - area.viewport().height())
+
+    def _get_scroll_area(self) -> QScrollArea | None:
+        parent = self.parentWidget()
+        while parent is not None:
+            if isinstance(parent, QScrollArea):
+                return parent
+            parent = parent.parentWidget()
+        return None
+
     def _init_colors(self) -> None:
         colors = self._config.hex_viewer_colors
         if colors:
             self._colors[self.Colors.NORMAL] = QColor(colors.get("normal", "#000000"))
             self._colors[self.Colors.HOVER] = QColor(colors.get("hover", "#0000FF"))
-            self._colors[self.Colors.SELECTED] = QColor(colors.get("selected", "#FF0000"))
+            self._colors[self.Colors.SELECTED] = QColor(colors.get("selected", "#FFFF00"))
             self._colors[self.Colors.NULL_VALUE] = QColor(colors.get("null_value", "#888888"))
+            self._colors[self.Colors.CHANGED] = QColor(colors.get("changed", "#FF0000"))
         else:
             palette = self.palette()
             self._colors[HexViewer.Colors.NORMAL] = palette.color(QPalette.ColorRole.WindowText)
             self._colors[HexViewer.Colors.HOVER] = palette.color(QPalette.ColorRole.Highlight)
             self._colors[HexViewer.Colors.SELECTED] = palette.color(QPalette.ColorRole.PlaceholderText)
             self._colors[HexViewer.Colors.NULL_VALUE] = palette.color(QPalette.ColorRole.Dark)
-            self._config.hex_viewer_colors = {k.name.lower(): v.name() for k, v in self._colors.items()}
+            self._colors[HexViewer.Colors.CHANGED] = palette.color(QPalette.ColorRole.Link)
+        self._config.hex_viewer_colors = {k.name.lower(): v.name() for k, v in self._colors.items()}
 
     def _calculate_size(self) -> SizeHint:
         font_metrics = QFontMetrics(self.font())
@@ -237,9 +274,19 @@ class HexViewer(QWidget):
                     color = self._colors[self.Colors.HOVER]
                 elif selected:
                     color = self._colors[self.Colors.SELECTED]
+
+                if self._data_source[byte_idx] != b:
+                    color = _blend_color(color, self._colors[self.Colors.CHANGED], 0.7)
+
                 painter.setPen(color)
                 pos = self._get_hex_position(byte_idx)
                 painter.drawText(pos, f'{b:02X}')
+
+                if selected and self.hasFocus():
+                    cursor_x = pos.x() + (self.fontMetrics().horizontalAdvance('0') if self._edit_cursor_pos == 1 else 0)
+                    cursor_y = pos.y() - self.fontMetrics().ascent()
+                    painter.drawLine(cursor_x, cursor_y, cursor_x, cursor_y + self.fontMetrics().height())
+
                 pos = self._get_ascii_position(byte_idx)
                 char = chr(b) if 32 <= b < 127 else '.'
                 painter.drawText(pos, char)
@@ -262,3 +309,117 @@ class HexViewer(QWidget):
     def wheelEvent(self, event: QWheelEvent) -> None:
         super().wheelEvent(event)
         QTimer.singleShot(0, self._update_hover_from_cursor)
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        if event.key() in HEX_EDIT_KEYS:
+            if self._selected_byte is not None:
+                byte_value = self._data[self._selected_byte]
+                if event.key() in NUMBER_KEYS:
+                    digit = event.key() - Qt.Key.Key_0
+                elif event.key() in LETTER_KEYS:
+                    digit = event.key() - Qt.Key.Key_A + 10
+                else:
+                    super().keyPressEvent(event)
+                    return
+                if self._edit_cursor_pos == 0:
+                    new_value = (byte_value & 0x0F) | (digit << 4)
+                else:
+                    new_value = (byte_value & 0xF0) | digit
+                self._data[self._selected_byte] = new_value
+
+                if self._edit_cursor_pos == 0:
+                    self._edit_cursor_pos = 1
+                else:
+                    self._edit_cursor_pos = 0
+                    if self._selected_byte + 1 < len(self._data):
+                        self._selected_byte += 1
+
+                self.byte_clicked.emit(self._selected_byte)
+                self.repaint()
+
+        elif event.key() == Qt.Key.Key_Escape:
+            self._set_selected_byte(None)
+        elif event.key() == Qt.Key.Key_Left:
+            if self._selected_byte is not None:
+                if self._edit_cursor_pos == 1:
+                    self._edit_cursor_pos = 0
+                    self.repaint()
+                elif self._selected_byte > 0:
+                    self._edit_cursor_pos = 1
+                    self._set_selected_byte(self._selected_byte - 1)
+            else:
+                self._set_selected_byte(len(self._data) - 1)
+        elif event.key() == Qt.Key.Key_Right:
+            if self._selected_byte is not None:
+                if self._edit_cursor_pos == 0:
+                    self._edit_cursor_pos = 1
+                    self.repaint()
+                elif self._selected_byte + 1 < len(self._data):
+                    self._edit_cursor_pos = 0
+                    self._set_selected_byte(self._selected_byte + 1)
+            else:
+                self._set_selected_byte(0)
+        elif event.key() == Qt.Key.Key_Up:
+            if self._selected_byte is not None:
+                size_hint = self._current_size_hint
+                if self._selected_byte >= size_hint.bytes_per_line:
+                    self._set_selected_byte(self._selected_byte - size_hint.bytes_per_line)
+            else:
+                self._set_selected_byte(0)
+        elif event.key() == Qt.Key.Key_Down:
+            if self._selected_byte is not None:
+                size_hint = self._current_size_hint
+                if self._selected_byte + size_hint.bytes_per_line < len(self._data):
+                    self._set_selected_byte(self._selected_byte + size_hint.bytes_per_line)
+            else:
+                self._set_selected_byte(0)
+        elif event.key() == Qt.Key.Key_Home:
+            if self._selected_byte is not None:
+                size_hint = self._current_size_hint
+                line_start = (self._selected_byte // size_hint.bytes_per_line) * size_hint.bytes_per_line
+                self._set_selected_byte(line_start)
+            else:
+                self._set_selected_byte(0)
+        elif event.key() == Qt.Key.Key_End:
+            if self._selected_byte is not None:
+                size_hint = self._current_size_hint
+                line_start = (self._selected_byte // size_hint.bytes_per_line) * size_hint.bytes_per_line
+                line_end = min(line_start + size_hint.bytes_per_line - 1, len(self._data) - 1)
+                self._set_selected_byte(line_end)
+            else:
+                self._set_selected_byte(len(self._data) - 1)
+        elif event.key() == Qt.Key.Key_PageUp:
+            if self._selected_byte is not None:
+                scroll_area = self._get_scroll_area()
+                if scroll_area is None:
+                    height = self.height()
+                else:
+                    height = scroll_area.viewport().height()
+                size_hint = self._current_size_hint
+                bytes_per_page = size_hint.bytes_per_line * (height // self.fontMetrics().height())
+                new_selected = self._selected_byte - bytes_per_page
+                if new_selected < 0:
+                    new_selected = 0
+                    self._edit_cursor_pos = 0
+                self._set_selected_byte(new_selected)
+            else:
+                self._set_selected_byte(0)
+        elif event.key() == Qt.Key.Key_PageDown:
+            if self._selected_byte is not None:
+                scroll_area = self._get_scroll_area()
+                if scroll_area is None:
+                    height = self.height()
+                else:
+                    height = scroll_area.viewport().height()
+                size_hint = self._current_size_hint
+                bytes_per_page = size_hint.bytes_per_line * (height // self.fontMetrics().height())
+                new_selected = self._selected_byte + bytes_per_page
+                self._set_selected_byte(min(new_selected, len(self._data) - 1))
+            else:
+                self._set_selected_byte(0)
+        else:
+            super().keyPressEvent(event)
+            return
+
+        self.ensure_selected_byte_visible()
+
